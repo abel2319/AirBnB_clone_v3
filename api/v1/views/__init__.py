960aacd0f05e28284a4af7ba826a3f6e58ed9fea
abel2319 <abelbakary@gmail.com>
2023-01-25 00:47:59 +0100

    task3

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""first endpoint (route) will be to return the status of your API
+"""
+
+
+from flask import Blueprint, render_template, abort
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""first endpoint (route) will be to return the status of your API
+"""
+
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def status():
+    """return ok if everything works
+    """
+    return ({ 'status': 'OK' })
