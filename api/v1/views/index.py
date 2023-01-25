f7c2fe98bf65a7eeca42ea47e50240e70fcecfa5
abel2319 <abelbakary@gmail.com>
2023-01-25 01:11:40 +0100

    task4 begin

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,14 @@
 """
 
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
@@ -10,3 +18,16 @@ def status():
     """return ok if everything works
     """
     return ({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def stats():
+    dict_stat = {
+            "amenities": strorage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            }
+    return (dict_stat)
